1c592e1be1969d6f131c478646f598ed5a8d0560
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-03-05 18:04:01 -0800

    add custom, simpler theme

--- docs/conf.py
+++ docs/conf.py
@@ -91,13 +91,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme_path = '.'
+html_theme = 'python_mock_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'nosidebar': True
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
